490fe37321a89651186395e408cf4981ce8649d4
danielsellergren <danielsellergren@gmail.com>
2023-03-08 22:19:02 -0500

    Adds add_item and remove_item routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "backpacks#index"
 
   resources :backpacks, only: [:index, :show]
+
+  post '/backpacks/:id/add/:item_id', to: 'backpacks#add_item', as: :add_item_backpack
+  delete '/backpackss/:id/remove/:item_id', to: 'backpacks#remove_item', as: :remove_item_backpack
 end
