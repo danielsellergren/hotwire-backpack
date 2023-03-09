69bd95e12be5528df414e2d934cb9d951caa8e0b
danielsellergren <danielsellergren@gmail.com>
2023-03-08 22:17:01 -0500

    Adds root and Backpacks resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'backpacks/index'
-  get 'backpacks/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "backpacks#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :backpacks, only: [:index, :show]
 end
